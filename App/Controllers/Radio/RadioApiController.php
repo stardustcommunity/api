4b63753b7611e5c4e422cfd84d7ba7eb04cd2369
Le_Futuriste <spamfree@matthieubessat.fr>
2018-04-29 14:32:14 +0200

    Fix time diff

--- App/Controllers/Radio/RadioApiController.php
+++ App/Controllers/Radio/RadioApiController.php
@@ -1,17 +1,39 @@
 <?php
+
 namespace App\Controllers\Radio;
 
 use App\Controllers\Controller;
 use Carbon\Carbon;
+use Carbon\CarbonInterval;
+use DusanKasan\Knapsack\Tests\Helpers\Car;
 use GuzzleHttp\Client;
 use Laravie\Parser\Xml\Document;
 use Laravie\Parser\Xml\Reader;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
-class RadioApiController extends Controller {
-	public function getInfo(ServerRequestInterface $request, ResponseInterface $response,Client $client){
-		$radionomyResponse = $client->get("http://api.radionomy.com/currentsong.cfm?radiouid={$this->container->get('radionomy')['radio_uid']}&type=xml&cover=yes&defaultcover=yes&size=90&dynamicconf=yes ");
+class RadioApiController extends Controller
+{
+	public function getInfo(ServerRequestInterface $request, ResponseInterface $response, Client $client)
+	{
+		try {
+			$radionomyResponse = $client->get(
+				"http://api.radionomy.com/currentsong.cfm?radiouid={$this->container->get('radionomy')['radio_uid']}&type=xml&cover=yes&defaultcover=yes&size=90&dynamicconf=yes",
+				[
+					'timeout' => 24
+				]
+			);
+		} catch (\Exception $e) {
+			return $response->withStatus(400)->withJson([
+				'success' => false,
+				'errors' => [
+					[
+						'code' => $e->getCode(),
+						'message' => $e->getMessage()
+					]
+				]
+			]);
+		}
 		$xml = (new Reader(new Document()))->extract($radionomyResponse->getBody()->getContents());
 		$track = $xml->parse([
 			'radionomy_id' => ['uses' => 'track.uniqueid'],
@@ -29,7 +51,8 @@ class RadioApiController extends Controller {
 			'title' => "Un titre",
 			'artists' => "Des artistes",
 		];*/
-		$endTime = Carbon::parse($track['start_time'])->addSeconds($track['play_duration'] / 1000);
+		$endTime = Carbon::parse($track['start_time'], 'Europe/Paris')->addSeconds($track['play_duration'] / 1000);
+
 		return $response->withJson([
 			'success' => true,
 			'data' => [
@@ -37,8 +60,9 @@ class RadioApiController extends Controller {
 				'time' => [
 					'end_at' => $endTime->toDateTimeString(),
 					'end_in' => [
+						'milliseconds' => $endTime->diffInSeconds() * 1000,
 						'seconds' => $endTime->diffInSeconds(),
-						'human' => $endTime->diffForHumans()
+						'human' => $endTime->diffForHumans(),
 					]
 				]
 			]
